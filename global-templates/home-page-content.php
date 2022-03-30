5058a835013da9fea371659be9cd52a86fbf0913
Sebastian Lomme <sebastian.lomme@gmail.com>
2022-03-30 16:27:56 +0200

    cards modifided

--- global-templates/home-page-content.php
+++ global-templates/home-page-content.php
@@ -3,23 +3,22 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4">
-                <div class="card" style="width: 18rem;">
+                <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <h5 class="card-title">Werving & Selectie</h5>
+                        <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
                             the
                             card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <a href="#" class="card-link">Lees meer...</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4">
-                <div class="card bg-light" style="width: 18rem;">
+                <div class="card bg-light">
                     <div class="card-body">
                         <h5 class="card-title">Werving & Selectie</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
                             the
                             card's content.</p>
@@ -29,10 +28,10 @@
                 </div>
             </div>
             <div class="col-lg-4">
-                <div class="card" style="width: 18rem;">
+                <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
                             the
                             card's content.</p>
