692f373dcca6f53afd00a357825a8f0a69f66f48
Sebastian Lomme <sebastian.lomme@gmail.com>
2022-03-31 14:58:49 +0200

    page.php added whit header

--- page.php
+++ page.php
@@ -24,7 +24,7 @@ $container = get_theme_mod( 'understrap_container_type' );
     <div class="hero-section">
         <div class="overlay"></div>
         <div class="hero-section__content container">
-            <?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+            <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
         </div> <!-- hero-section__content -->
     </div> <!-- hero-section -->
 
