ec62333b421d1ff9dd48b1b473d2dfe08009cdcb
Sebastian Lomme <sebastian.lomme@gmail.com>
2022-06-03 14:32:51 +0200

    open sollicitatie button

--- js/child-theme.js
+++ js/child-theme.js
@@ -9296,19 +9296,6 @@
 	  }
 	})();
 
-	// Add your custom JS here.
-	const closeButton = document.getElementById("close-button-popup");
-	const popUpContainer = document.getElementById("pop-up-container");
-	const openSollicitatieButton = document.getElementById("menu-item-66");
-	closeButton.addEventListener("click", () => {
-	  console.log("Click");
-	  popUpContainer.classList.add("hide");
-	});
-	openSollicitatieButton.addEventListener("click", () => {
-	  console.log("Click");
-	  popUpContainer.classList.remove("hide");
-	});
-
 	exports.Alert = alert;
 	exports.Button = button;
 	exports.Carousel = carousel;
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,19 +1,19 @@
 // Add your custom JS here.
 
-const closeButton = document.getElementById("close-button-popup")
-const popUpContainer = document.getElementById("pop-up-container")
-const openSollicitatieButton = document.getElementById("menu-item-66")
+// const closeButton = document.getElementById("close-button-popup")
+// const popUpContainer = document.getElementById("pop-up-container")
+// const openSollicitatieButton = document.getElementById("menu-item-66")
 
-closeButton.addEventListener("click", () => {
-    console.log("Click")
-    popUpContainer.classList.add("hide")
-})
+// closeButton.addEventListener("click", () => {
+//     console.log("Click")
+//     popUpContainer.classList.add("hide")
+// })
 
 
-openSollicitatieButton.addEventListener("click", () => {
-    console.log("Click")
-    popUpContainer.classList.remove("hide")
-})
+// openSollicitatieButton.addEventListener("click", () => {
+//     console.log("Click")
+//     popUpContainer.classList.remove("hide")
+// })
 
 
 
