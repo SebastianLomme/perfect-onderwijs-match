ccf41b26a4edc471a0d6924d006320cb8f164f07
Sebastian Lomme <sebastian.lomme@gmail.com>
2022-06-06 23:11:43 +0200

    float label

--- js/child-theme.js
+++ js/child-theme.js
@@ -9296,6 +9296,45 @@
 	  }
 	})();
 
+	// Add your custom JS here.
+	// const closeButton = document.getElementById("close-button-popup")
+	// const popUpContainer = document.getElementById("pop-up-container")
+	// const openSollicitatieButton = document.getElementById("menu-item-66")
+	// closeButton.addEventListener("click", () => {
+	//     console.log("Click")
+	//     popUpContainer.classList.add("hide")
+	// })
+	// openSollicitatieButton.addEventListener("click", () => {
+	//     console.log("Click")
+	//     popUpContainer.classList.remove("hide")
+	// })
+	document.addEventListener('DOMContentLoaded', event => {
+	  const fields = document.getElementsByClassName("gfield");
+	  Array.from(fields).forEach(field => {
+	    const input = field.querySelector("input[type='text'], input[type='email'], input[type='tel'], textarea");
+	    const label = field.querySelector("label");
+
+	    if (input) {
+	      input.addEventListener("input", () => {
+	        if (input.value) {
+	          console.log("input");
+	          console.log("value", input.value);
+	          label.classList.add("focus-label");
+	        } else {
+	          label.classList.remove("focus-label");
+	        }
+	      });
+	    }
+
+	    if (input && input.value) {
+	      label.classList.add("focus-label");
+	      console.log("test");
+	    } else if (input && !input.value) {
+	      label.classList.remove("focus-label");
+	    }
+	  }); //the event occurred
+	});
+
 	exports.Alert = alert;
 	exports.Button = button;
 	exports.Carousel = carousel;
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -17,4 +17,30 @@
 
 
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    const fields = document.getElementsByClassName("gfield")
+
+    Array.from(fields).forEach(field => {
+        const input = field.querySelector("input[type='text'], input[type='email'], input[type='tel'], textarea")
+        const label = field.querySelector("label")
+        if (input) {
+            input.addEventListener("input", () => {
+                if (input.value) {
+                    console.log("input")
+                    console.log("value", input.value)
+                    label.classList.add("focus-label")
+                } else {
+                    label.classList.remove("focus-label")
+                }
+            })
+        } if (input && input.value) {
+            label.classList.add("focus-label")
+            console.log("test")
+        } else if (input && !input.value) {
+            label.classList.remove("focus-label")
+        }
+    })
+    //the event occurred
+})
+
 
